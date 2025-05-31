e9f269a145f39a665a456c9df2e5d17368824a42
ahmednagy-work <ahmad.n@granadapride.com>
2025-05-31 23:11:03 +0300

    add dark theme to the website

--- app.py
+++ app.py
@@ -220,9 +220,9 @@ def serve_video(filename):
 
 #     return send_from_directory(OUTPUT_DIR, filename, as_attachment=True)
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
-
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT",6000))
-    app.run(host="0.0.0.0",port=port)
+    app.run(debug=True)
+
+# if __name__ == "__main__":
+#     port = int(os.environ.get("PORT",6000))
+#     app.run(host="0.0.0.0",port=port)
